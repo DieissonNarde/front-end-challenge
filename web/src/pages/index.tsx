20bb64425f44e7c1a918fd62bbaf39448a3e1f6c
DieissonNarde <dieissonnarde@gmail.com>
2022-05-22 17:59:20 -0400

    initial setup

---
+++ web/src/pages/index.tsx
@@ -0,0 +1,18 @@
+import type { NextPage } from 'next';
+import Head from 'next/head';
+
+const Home: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <title>TMDB</title>
+      </Head>
+
+      <main>
+        <h1>Home</h1>
+      </main>
+    </>
+  )
+}
+
+export default Home;

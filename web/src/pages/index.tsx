14300da7ad199bb6e249a9309f0c0027fcebb8b9
DieissonNarde <dieissonnarde@gmail.com>
2022-07-21 18:52:30 -0400

    feat: add home page

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -1,12 +1,15 @@
+import { useState } from 'react';
 import type { NextPage } from 'next';
 import Head from 'next/head';
-import { useState } from 'react';
+import { Button } from '../components/Button';
+import { Card } from '../components/Card';
 
 
 import { Header } from '../components/Header';
 import { Pagination } from '../components/Pagination';
+import { CardWrapper, CategoryWrapper, HeaderContainer, HeaderContent, HomeContent, HomeSection } from '../styles/pages/home';
 
-let PageSize = 6;
+let PageSize = 10;
 
 const Home: NextPage = () => {
   const [currentPage, setCurrentPage] = useState(1);
@@ -19,15 +22,39 @@ const Home: NextPage = () => {
 
       <main>
         <Header />
-        <div>
+        <HomeSection>
+          <HeaderContainer>
+            <HeaderContent>
+              <h3>Milhões de filmes, séries e pessoas para descobrir. Explore já.</h3>
+              <p>FILTRE POR:</p>
+              <CategoryWrapper>
+                <Button>Teste</Button>
+              </CategoryWrapper>
+            </HeaderContent>
+          </HeaderContainer>
+
+          <HomeContent>
+            <CardWrapper>
+              <Card />
+              <Card />
+              <Card />
+              <Card />
+              <Card />
+              <Card />
+              <Card />
+              <Card />
+              <Card />
+            </CardWrapper>
+          </HomeContent>
+
           <Pagination
             currentPage={currentPage}
             totalCount={80}
             pageSize={PageSize}
+            siblingCount={1}
             onPageChange={(page: number) => setCurrentPage(page)}
           />
-        </div>
-
+        </HomeSection>
       </main>
     </>
   )
---
+++ web/src/styles/pages/home.ts
@@ -0,0 +1,92 @@
+import styled from 'styled-components';
+
+export const HomeSection = styled.section`
+  width: 100%;
+  margin-bottom: 12rem;
+`;
+
+export const HeaderContainer = styled.header`
+  background-color: #2D0C5E;
+  padding: 4rem 1.6rem 4.7rem;
+
+  @media (min-width: 1024px) {
+    padding-block: 8.5rem;
+  }
+`;
+
+export const HeaderContent = styled.div`
+  width: 100%;
+
+  > h3 {
+    font-weight: 700;
+    font-size: 2.4rem;
+    line-height: 2.8rem;
+    color: #FFFFFF;
+    margin-bottom: 3.6rem;
+  }
+
+  > p {
+    font-weight: 700;
+    font-size: 1.4rem;
+    line-height: 2rem;
+    color: #FFFFFF;
+    margin-bottom: .8rem;
+  }
+
+  @media (min-width: 1024px) {
+    max-width: 100rem;
+    margin: 0 auto;
+    text-align: center;
+
+    > h3 {
+      font-size: 4.8rem;
+      line-height: 5.6rem;
+      letter-spacing: -0.005em;
+      margin-bottom: 4rem;
+      max-width: 78.1rem;
+      margin-inline: auto;
+    }
+
+    > p {
+      margin-bottom: 1.6rem;
+    }
+  }
+`;
+
+export const CategoryWrapper = styled.div`
+  display: inline-flex;
+  flex-wrap: wrap;
+  gap: 1.2rem;
+
+  @media (min-width: 1024px) {
+    justify-content: center;
+  }
+`;
+
+export const HomeContent = styled.div`
+  padding: 3.2rem 1.6rem 10.4rem;
+
+  @media (min-width: 1024px) {
+    padding: 2.9rem 11.2rem 5.4rem;
+  }
+`;
+
+export const CardWrapper = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  gap: 1.6rem;
+  justify-content: center;
+
+  > div {
+    margin-bottom: 2.4rem;
+  }
+
+  @media (min-width: 1024px) {
+    gap: 3.2rem;
+    justify-content: flex-start;
+
+    > div {
+      margin-bottom: 1.6rem;
+    }
+  }
+`;

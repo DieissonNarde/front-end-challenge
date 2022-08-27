b3c600090d95d5b17e818b1c7d1fc604796a5f1d
DieissonNarde <dieissonnarde@gmail.com>
2022-08-27 17:25:55 -0400

    fix: fix google font import and change description

--- web/src/pages/_document.tsx
+++ web/src/pages/_document.tsx
@@ -41,13 +41,14 @@ export default class MyDocument extends Document {
     return (
       <Html lang="pt-br">
         <Head>
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" />
-
+          <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap" rel="stylesheet" />
           <meta charSet="utf-8" />
           <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
 
-          <meta name="description" content="Teste front-end Promobit" />
-          <link rel="icon" href="" />
+          <meta name="description" content="Site para busca de novos filmes por listagem de mais populares do dia e filtragem por gênero" />
+          {/* <link rel="icon" href="" /> */}
         </Head>
         <body>
           <Main />
